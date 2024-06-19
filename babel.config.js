2fbe464e55fd4a0e732e5f42d6bed6a08277fdf9
Autumn <autumn.1010.shin@gmail.com>
2024-06-19 13:39:29 -0700

    second

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+=======
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo']
+>>>>>>> 8f5e114 (before nav)
   };
 };
